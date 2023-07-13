44d2b7929b96e79d114c72121c4b5f209446517e
lucqaz01 <Lucassobczak3@gmail.com>
2023-07-12 20:51:58 -0400

    Basic menu selection and first func.

---
+++ main.c
@@ -0,0 +1,111 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SALES 6
+#define PRODUCTIVITY 3
+#define INVENTORY 4
+
+typedef struct
+{
+    int num;
+    char * option;
+    float (*operation)(void);
+}
+    menuOption;
+
+char selectOption(menuOption menuOption[], int optionSize);
+float sellThroughRate(void);
+
+menuOption operationSelection[] = {
+    {1, "sales metrics"},
+    {2, "productivity metrics"},
+    {3, "inventory metrics"},
+};
+
+menuOption salesMetrics[] = {
+    {1, "average dollar sales", sellThroughRate},
+    {2, "average per week", NULL}, 
+    {3, "average order value", NULL},
+    {4, "average unit cost", NULL},
+    {5, "average unit retail", NULL},
+    {6, "cost of goods sold", NULL},
+};
+
+menuOption productivityMetrics[] = {
+    {1, "sell through rate", sellThroughRate},
+    {2, "gross margin", NULL}, 
+    {3, "gross margin return on investment", NULL},
+};
+
+menuOption inventoryMetrics[] = {
+    {1, "average inventory", NULL},
+    {2, "stock to sales", NULL}, 
+    {3, "turn", NULL},
+    {4, "weeks of supply", NULL},
+};
+
+int main(void)
+{
+   char option = selectOption(operationSelection, sizeof(operationSelection) / sizeof(operationSelection[0]));
+    int optionIndex;
+
+    switch (option) 
+    {   
+        case '1':
+            
+            option = selectOption(salesMetrics, sizeof(salesMetrics) / sizeof(salesMetrics[0]));
+            optionIndex = option - '1';
+
+            if (optionIndex >= 0 && optionIndex < sizeof(salesMetrics) / sizeof(salesMetrics[0]))
+            {
+                if (salesMetrics[optionIndex].operation != NULL)
+                {
+                    salesMetrics[optionIndex].operation();
+                }
+                else
+                {
+                    printf("No operation assigned to this option.\n");
+                }
+            }
+            else
+            {
+                printf("Invalid option.\n");
+            }
+            break;
+
+        default:
+            printf("Please select a valid option\n");
+            option = selectOption(operationSelection, sizeof(operationSelection[0]));
+    }
+    return 0;
+};
+
+char selectOption(menuOption menuOption[], int optionSize)
+{
+    char selected;
+
+    printf("Please select an option below:\n");
+    for (int i = 0; i < optionSize; i++)
+    {
+        printf("%i. %s\n", menuOption[i].num, menuOption[i].option);
+    }
+    scanf("%c", &selected);
+    
+    return selected;
+};
+
+float sellThroughRate(void)
+{
+    int unitsSold, unitsBought;
+
+    printf("How many units have you sold?\n");
+    scanf("%i", &unitsSold);
+
+    printf("How many units did you buy in total?\n");
+    scanf("%i", &unitsBought);
+
+    printf("Your sell through rate is: %f\n", (float) unitsSold / unitsBought);
+    return 1;
+};
